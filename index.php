6af2cf97e34e81d246906b18fcf2d69d23724bee
husencoolwolf <52161767+husencoolwolf@users.noreply.github.com>
2021-11-24 12:02:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,29 +11,47 @@
 </head>
 
 <body class="bg-light">
+  <!-- Navbar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">ARTURE FURNITURE</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+    <!--  Show this only on mobile to medium screens  -->
+    <a class="navbar-brand d-lg-none" href="#">
+      <img class="" src="/fargasa/assets/Fargasa Logo Circle.png" alt="Dashboard" style="width: 80px">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+
+    <!--  Use flexbox utility classes to change how the child elements are justified  -->
+    <div class="collapse navbar-collapse justify-content-between" id="navbarToggle">
+
+      <!--   Show this only lg screens and up   -->
       <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="navbar-brand d-none d-lg-block" href="#">
+          <img class="" src="/fargasa/assets/Fargasa Logo Circle.png" alt="Dashboard" style="width: 80px">
+        </a>
+      </ul>
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item px-3 ml-0">
+          <a class="nav-link active" href="#catalog">CATALOG <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+        <li class="nav-item px-3 ml-0">
+          <a class="nav-link" href="#carouselExampleControls">PROMO</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+        <li class="nav-item px-3 ml-0">
+          <a class="nav-link" href="#profil">PROFIL</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+        <li class="nav-item px-3 ml-0">
+          <a class="nav-link" href="#contact">HUBUNGI KAMI</a>
         </li>
       </ul>
+
     </div>
   </nav>
 </body>
+<script src="dist/js/jquery-3.5.1.js"></script>
+<script src="dist/js/bootstrap.js"></script>
+
 
 </html>
