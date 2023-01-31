265e122006a88f0d7ee0afb67ae635c08e051ae8
Mohammad Ali Vellayati Husaini <52161767+husencoolwolf@users.noreply.github.com>
2023-01-31 10:15:30 +0700

    Update telegram.php

--- api/telegram.php
+++ api/telegram.php
@@ -12,7 +12,6 @@ class Telegram
   {
     $data = json_decode($data);
     $this->_telegramData = $data;
-    // $this->_telegramData->token = "5438148499:AAEoXDPqr_A5ihXyD_2nfrZ7zSrzHwJQHkc";
   }
 
   function __getData()
